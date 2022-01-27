4369332f068866f707d5feaddebc030cc6211731
LarisaGimbu <gimbularisa@gmail.com>
2022-01-27 15:38:29 +0100

    paginate

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Comic;
 use Illuminate\Http\Request;
 
 class ComicController extends Controller
@@ -13,7 +14,9 @@ class ComicController extends Controller
      */
     public function index()
     {
-        return view ('comics.home');
+        $comics = Comic::paginate(4);
+
+        return view ('comics.home', compact('comics'));
     }
 
     /**
--- resources/views/comics/home.blade.php
+++ resources/views/comics/home.blade.php
@@ -2,6 +2,33 @@
 
 @section('content')
   <div class="container">
-    <h1>Comics</h1>
+    <h1>Comics:</h1>
+
+    <table class="table table-dark table-hover">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Title</th>
+          <th scope="col">Type</th>
+          <th scope="col">Price</th>
+          <th scope="col">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        @forelse ($comics as $comic)
+        <tr>
+          <th scope="row"> {{$comic->id}} </th>
+          <td>{{$comic->title}}</td>
+          <td>{{$comic->type}}</td>
+          <td>{{$comic->price}}</td>
+        </tr>
+        @empty
+          <h3>Non sono stati trovati fumetti</h3>
+        @endforelse
+        
+      </tbody>
+    </table>
+
+    {{$comics->links()}}
   </div>
 @endsection
