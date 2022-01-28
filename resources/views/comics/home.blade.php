dc3b8ee81770f0b2a621f05fb4db0ea8a299d42d
LarisaGimbu <gimbularisa@gmail.com>
2022-01-28 19:14:11 +0100

    delete

--- resources/views/comics/home.blade.php
+++ resources/views/comics/home.blade.php
@@ -11,7 +11,7 @@
           <th scope="col">Title</th>
           <th scope="col">Type</th>
           <th scope="col">Price</th>
-          <th scope="col">Actions</th>
+          <th colspan="3" scope="col">Actions</th>
         </tr>
       </thead>
       <tbody>
@@ -23,7 +23,14 @@
           <td>{{$comic->price}}</td>
           <td>
             <a class="btn-warning p-2 rounded" href=" {{route('comics.show', $comic)}} ">Show</a>
-            <a class="btn-success p-2 rounded" href=" {{route('comics.edit', $comic)}} ">Edit</a>
+          </td>
+          <td><a class="btn-success p-2 rounded" href=" {{route('comics.edit', $comic)}} ">Edit</a></td>
+          <td>
+            <form action=" {{route('comics.destroy', $comic)}}" method="POST">
+              @csrf
+              @method('DELETE')
+              <button class="btn btn-danger p-2 rounded" type="submit">Delete</button>
+            </form>
           </td>
         </tr>
         @empty
